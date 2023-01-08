0d42207d79c782caae31afdd9224ba4a0abfb1a9
David iMac <david@digitalgeo.com.au>
2023-01-08 10:00:15 +0800

    Converted Ex1 from EF6 to EFCore

--- EFCore/Ex1_ModelPerson/frmPerson.cs
+++ EFCore/Ex1_ModelPerson/frmPerson.cs
@@ -1,10 +1,8 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Data.Entity.Validation;
-//using System.Data.Entity;
-//using System.Data.Entity.Validation;
 using System.Drawing;
 using System.Globalization;
 using System.Linq;
@@ -13,45 +11,52 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using Microsoft.EntityFrameworkCore;
 
-namespace Ex1_Person
+namespace Ex1_ModelPerson
 {
-    public partial class frmPerson : Form
+    public partial class FrmPerson : Form
     {
-        //How to use Entity Framework
-        //https://docs.microsoft.com/en-us/ef/ef6/
-
-        public frmPerson()
+        public FrmPerson()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void FrmPerson_Load(object sender, EventArgs e)
         {
-            using (PersonModel context = new PersonModel())
-            {
-                txtConnection.Text = context.Database.Connection.ConnectionString;
-            }
-
+            txtConnection.Text = PersonModel.ConnectionString;
         }
 
         private void btnTestPerson_Click(object sender, EventArgs e)
         {
             TestPerson();
         }
-
         private void TestPerson()
         {
             txtDebug.Text = "TestPerson()\r\n";
 
-            using (PersonModel context = new PersonModel())
+            //.Net Core has IoC (Inversion Of Control) implemented in it's roots and uses Dependency Injection, Instance pooling 
+            // This means; you don't create a context, you ask the framework to give you one, based on some rules you defined before.
+            // Unfortunatley, using DI enables many things, true, including complexity.. this is beyond this basic demos requirements.
+
+            // So for this we use, a constructor  For this example we create a dbcontext object on request and dynamically point it to the user's individual database
+            // Sometimes requirements, time or money constraints, quality attributes or anything else 
+
+            // This is a way to "just a way to instantiate" the object, activator's CreateInstance
+            var optionsBuilder = new DbContextOptionsBuilder<PersonModel>();
+            optionsBuilder.UseSqlServer(PersonModel.ConnectionString);
+            optionsBuilder.LogTo(Console.WriteLine);
+
+            using (PersonModel context = new PersonModel(optionsBuilder.Options))
             {
                 // Perform data access using the context
-                context.Database.Log = Console.Write;
 
-                context.Database.Delete();
+                // context.Database.Log = Console.Write; // Done in options above
+
+                context.Database.EnsureDeleted(); //.Delete
+
                 txtDebug.Text += "Deleted DB\r\n";
 
-                context.Database.CreateIfNotExists();
+                context.Database.EnsureCreated(); //.CreateIfNotExists();
+
                 txtDebug.Text += "Created DB\r\n";
 
                 Person person = new Person();
@@ -63,7 +68,6 @@ namespace Ex1_Person
                 person.DOB = Convert.ToDateTime("6/12/70", culture);
 
                 context.People.Add(person);
-
 
                 // This Exception handler helps to describe what went wrong with the EF database save.
                 // It decodes why the data did not comply with defined database field structure. e.g too long or wrong type.
@@ -94,11 +98,6 @@ namespace Ex1_Person
                 foreach (Person p in people)
                     txtDebug.Text += String.Format("{0} {1} {2} {3} {4}", p.Id, p.FirstName, p.MiddleName, p.LastName, p.Phone) + "\r\n";
             }
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
         }
     }
 }
