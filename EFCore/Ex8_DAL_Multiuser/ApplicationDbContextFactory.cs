abab42743a66decd28dd87406411f3347da34bdc
David V <david@digitalgeo.com.au>
2023-05-01 21:27:47 +0800

    WIP adding Ex 8

---
+++ EFCore/Ex8_DAL_Multiuser/ApplicationDbContextFactory.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Entity.Core.Common.CommandTrees.ExpressionBuilder;
+using System.Dynamic;
+
+namespace Ex8_DAL_Multiuser
+{
+
+    // Some of the EF Core Tools commands(for example, the Scaffolding commands) require
+    // a derived DbContext instance to be created at design time in order to gather details about the application's entity types
+    // and how they map to a database schema. 
+
+    // In most cases, it is desirable that the DbContext thereby created is configured in a similar way to how it would be configured at run time.
+    /*
+        The tools first try to obtain the service provider by invoking
+
+        Program.CreateHostBuilder(),
+        calling Build(),
+        then accessing the Services property.
+
+        Use a design-time factory, needs to be in DAL project (e.g Ex7_DAL), not in API project.
+    */
+        using Microsoft.EntityFrameworkCore.Design;
+        using Microsoft.EntityFrameworkCore;
+    using Ex8_DAL_Multiuser;
+
+    namespace Ex7_DAL
+    {
+        public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<EFToDo>
+        {
+            public EFToDo CreateDbContext(string[] args)
+            {
+                var optionsBuilder = new DbContextOptionsBuilder<EFToDo>();
+                optionsBuilder.UseSqlServer(EFToDo.ConnectionString);
+
+                return new EFToDo(optionsBuilder.Options);
+            }
+        }
+    }
+}
---
+++ EFCore/Ex8_EF_WebApi_SigR/Program.cs
@@ -0,0 +1,47 @@
+using System.Diagnostics;
+using Ex8_DAL_Multiuser;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+DbContextOptionsBuilder<EFToDo> optionsBuilder;
+optionsBuilder = new DbContextOptionsBuilder<EFToDo>();
+optionsBuilder.UseSqlServer(EFToDo.ConnectionString);
+
+//Spin up Database
+if (Debugger.IsAttached)
+{
+    using (EFToDo db = new EFToDo(optionsBuilder.Options))
+    {
+        db.Database.EnsureDeleted();
+        Debug.WriteLine("Deleted DB\r\n");
+
+        db.Database.EnsureCreated();
+        Debug.WriteLine("Created DB\r\n");
+    }
+
+    //SeedData();
+}
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
